0c671abf5348010e6d4697f5185e6e79c797f2c9
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-05-24 18:49:25 -0500

    Se añaden 2 funciones

--- clase_4/tarea/index.js
+++ clase_4/tarea/index.js
@@ -29,7 +29,99 @@ function elemento(numero){
     return numero
 }
 
-const resultados = filter(arreglo, elemento)
-  console.log(resultados)
+//const resultados = filter(arreglo, elemento)
+//console.log(resultados)
+
+
+// Ejemplo:
+
+// const inventario = [
+//     {nombre: 'manzanas', cantidad: 2},
+//     {nombre: 'bananas', cantidad: 0},
+//     {nombre: 'cerezas', cantidad: 5}
+// ];
+
+// function esCereza(fruta) {
+//     return fruta.nombre === 'cerezas';
+// }
+
+// console.log(inventario.find(esCereza));
+// // { nombre: 'cerezas', cantidad: 5 }
+
+
+function find(array, callback){
+    const resultados =[];
+    for (let i=0; i<array.length; i++){
+        const resultado = callback(array[i])
+        if(callback(array[i]==="limon")){
+            resultadoFruta=[
+                {
+                    fruta: array[i].fruta,
+                    cantidad: array[i].cantidad
+                }
+            ]
+        }
+        resultados.push(resultadoFruta)
+    }
+    return resultados
+}
+
+const arregloFrutas =[
+    {
+        fruta: "manzana",
+        cantidad: 10,
+    },
+    {
+        fruta: "limon",
+        cantidad: 50,
+    },
+    {
+        fruta: "naranja",
+        cantidad: 5,
+    },
+    {
+        fruta: "melones",
+        cantidad: 3,
+    }
+    
+]
+
+function tipoFrutas(fruta){
+return fruta
+}
+
+//const resultados=find(arregloFrutas, tipoFrutas)
+//co0nsole.log(resultados)
+
+
+//Every 
+
+
+function every(array, callback){
+    const resultadosEvery ="";
+    for (let i=0; i<array.length; i++){
+        const resultadoEvery = callback(array[i])
+        if(callback(array[i]<0)){
+            resultadosEvery == "True"
+        }
+        else{
+            resultadosEvery =="False"
+        }
+    }
+    return resultadosEvery
+}
+
+const arregloEvery = [-5, -7, -6, -156, -8, -3, -20, -50];
+
+function elementoEvery(numeroEvery){
+    return numeroEvery
+}
+
+const resultadosEvery = every(arregloEvery, elementoEvery)
+console.log(resultadosEvery)
+
+
+
+
 
 
